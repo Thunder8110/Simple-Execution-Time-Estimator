586242fb919b7a28aab26e128a418ae5a74089b6
Thunder8110 <44309219+Thunder8110@users.noreply.github.com>
2023-06-21 09:31:03 +0900

    Update script.js
    defined elements

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ window.addEventListener("load", function () {
     elm.contentEditable = true;
   });
 });
+
+let addRowButton = document.getElementById("addRow")
+let remRowButton = document.getElementById("remRow")
+let calcButton = document.getElementById("calc")
+let inputTable = document.getElementById("input")
+let orderSelect = document.getElementById("order")
+let estSizeInput = document.getElementById("estSize")
+let resultA = document.getElementById("resA")
+let resultB = document.getElementById("resB")
+let resultTime = document.getElementById("resTime")
+let func = document.getElementById("func")
+
+function addRow() {
+  // TODO
+}
+
+function remRow() {
+  // TODO
+}
+
+function calculate() {
+  // TODO
+}
